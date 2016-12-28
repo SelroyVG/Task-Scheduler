c3a6d0cd886cf24a94d03e07c4c055a74bec877f
Georgy <egor@slizatest.code-geek.ru>
2016-12-28 16:19:21 +0300

    Updated

--- scheduler.cpp
+++ scheduler.cpp
@@ -25,32 +25,31 @@ struct config {
   string mysql_unix_socket;
 } config = {"127.0.0.1","scc","ZQLf-0.4","tasks_scheduler",0,""};
 
-int Daemon(int launchTime, string daemonCommand){
-	OpenSQL(config.mysql_host.c_str(),config.mysql_user.c_str(),config.mysql_passwd.c_str(), config.mysql_db.c_str(), config.mysql_port,config.mysql_unix_socket.c_str());
-	wSQL("INSERT INTO daemons_log (command,status,launch_time) VALUES(%q,%q,FROM_UNIXTIME(%u))",daemonCommand.c_str(),"inserted",launchTime);
-	
+int Daemon(int launchTime, string daemonCommand, bool restoreTasks){
+	if (!restoreTasks){
+		wSQL("INSERT INTO daemons_log (command,status,launch_time) VALUES(%q,%q,FROM_UNIXTIME(%u))",daemonCommand.c_str(),"inserted",launchTime);
+	}
 	time_t now;
 	time(&now);
 	int timeDelta = launchTime - now;
-	ofstream fout_command(".logs/output", std::ios_base::app);
 	if(timeDelta < 0){
 		string query = "UPDATE daemons_log SET status=\'denied\' WHERE command=\'" + daemonCommand + "\' AND launch_time=FROM_UNIXTIME(" + to_string(launchTime) + ") AND status=\'inserted\'";
 		wSQL(query.c_str());
 	}
-	else if (timeDelta > 0){
+	else if (timeDelta > 0){ 
 		sleep(timeDelta - 1);
 		
 		string query = "UPDATE daemons_log SET status=\'executed\' WHERE command=\'" + daemonCommand + "\' AND launch_time=FROM_UNIXTIME(" + to_string(launchTime) + ") AND status=\'inserted\'";
 		wSQL(query.c_str());
-		
+		ofstream fout_command(".logs/output", std::ios_base::app);
 		fout_command << "*** Command: \"" << daemonCommand << "\" Executed at: " << launchTime << " with results below. ***" << endl;
 		
 		daemonCommand += " >> .logs/output";
 		system(daemonCommand.c_str());
 		fout_command << "*** End of the output. ***" << endl << endl;
+		fout_command.close();
 		
 	}
-	fout_command.close();
 	CloseSQL();
 	return 0;
 }
@@ -114,17 +113,22 @@ int main(int argc, char ** argv) {
 	string query;
 	string daemonCommand;
 	string time;
+	bool restoreTasks = false;
+	
 	int launchTime = 0;
 	for (int argCounter = 1; argCounter < argc; argCounter++) 
 	{
 		string currentAgrument = argv[argCounter];
 		
+		if ((currentAgrument == "-r") || (currentAgrument == "--restore")){
+			restoreTasks = true;
+			continue;
+		}
+		
+		
 		if ((currentAgrument == "-c") || (currentAgrument == "--command")){
 			daemonCommand = argv[argCounter + 1];
-			for (int i = argCounter + 2; i < argc; i++){
-				daemonCommand = daemonCommand + " " + argv[i];
-			}
-			break;
+			continue;
 		}
 		if ((currentAgrument == "-t") || (currentAgrument == "--time")){
 			time = argv[argCounter + 1];
@@ -137,35 +141,38 @@ int main(int argc, char ** argv) {
 		}
 	}
 	
-	
-	if(daemonCommand.length() == 0){
-		cout << "Error: command not specified!" << endl;
-		return 1;
-	}
-	if(launchTime == 0){
-		cout << "Error: launch time not specified!" << endl;
-		return 1;
+	if (!restoreTasks){
+		if(daemonCommand.length() == 0){
+			cout << "Error: command not specified!" << endl;
+			return 1;
+		}
+		if(launchTime == 0){
+			cout << "Error: launch time not specified!" << endl;
+			return 1;
+		}
 	}
 	
 	pid_t pid = fork();
 	
 	if (pid == -1)
-    {
-        printf("Error: Start Daemon failed (%s)\n", strerror(errno));
-        
         return -1;
-    }
     else if (pid)
-    {
 		return 0;
-    }
 	
+	OpenSQL(config.mysql_host.c_str(),config.mysql_user.c_str(),config.mysql_passwd.c_str(), config.mysql_db.c_str(), config.mysql_port,config.mysql_unix_socket.c_str());
 	
 	umask(0);
     setsid();
-	//system ("cd /");    
-/*	close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);*/
-    return Daemon (launchTime, daemonCommand);
+	if (!restoreTasks){
+		Daemon (launchTime, daemonCommand, restoreTasks);
+	}
+	else{
+		const char * command;
+		 long long unsigned launch_time;
+		for (void * r = rfSQL("SELECT command,unix_timestamp(launch_time) FROM daemons_log WHERE status='inserted'"); rnSQL(r,&command, &launch_time);){
+			string query = "./taskscheduler -t " + to_string(launch_time) + " -c \"" + command + "\"";
+			system(query.c_str());
+		}
+	}
+	return 0;
 }
